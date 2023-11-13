8f5a060202ac2215db16fe3a721d7e481690b6b6
22PROGB07 <22PROGB07@TS-LABS-4.labs.cbtis94.local>
2023-11-13 08:33:42 -0600

    Trabajo Final Enfermeria

---
+++ src/cafeteria/JLIST.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cafeteria;
+
+/**
+ *
+ * @author 22PROGB07
+ */
+class JLIST {
+
+    static void addElement(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void remove(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void removeAllElements() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
